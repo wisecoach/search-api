0a8968c7998ee8b6063a87b02d893986205728fc
wisecoach <827165648@qq.com>
2021-03-07 16:41:01 +0800

    init springboot

---
+++ src/main/java/com/watering/ApiMain8081.java
@@ -0,0 +1,11 @@
+package com.watering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMain8081 {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMain8081.class);
+    }
+}
