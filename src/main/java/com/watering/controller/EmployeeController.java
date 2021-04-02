aa14d66c43c2df016ad27f4a363b9e8ceb532491
wisecoach <827165648@qq.com>
2021-04-02 16:44:43 +0800

    添加了employee的两个测试

---
+++ src/main/java/com/watering/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.watering.controller;
+
+import com.watering.entity.DTO.ResponseDTO;
+import com.watering.entity.EmployeeEntity;
+import com.watering.mapper.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    @Autowired
+    private EmployeeMapper employeeMapper;
+
+    @RequestMapping("/{id}")
+    public ResponseDTO find(@PathVariable("id") Integer id){
+        EmployeeEntity employeeEntity = employeeMapper.find(id);
+        return ResponseDTO.succData(employeeEntity);
+    }
+
+    // "到时候会用pagehelper，老子不会"
+    @Deprecated
+    @RequestMapping("/list")
+    public ResponseDTO findList(){
+        List<EmployeeEntity> list = employeeMapper.findList();
+        return ResponseDTO.succData(list);
+    }
+}
---
+++ src/main/java/com/watering/mapper/EmployeeMapper.java
@@ -0,0 +1,19 @@
+package com.watering.mapper;
+
+import com.watering.entity.EmployeeEntity;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface EmployeeMapper {
+    @Select("select * from employee where id = #{id}")
+    EmployeeEntity find(@Param("id") Integer id);
+
+    @Select("select * from employee limit 5")
+    List<EmployeeEntity> findList();
+}
