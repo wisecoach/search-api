b264709f007d068fe2a5bd42635ddc76cf973e87
wisecoach <827165648@qq.com>
2021-04-07 21:42:48 +0800

    给阿房加了跨域，修改了searchController

---
+++ src/main/java/com/watering/controller/SearchController.java
@@ -0,0 +1,23 @@
+package com.watering.controller;
+
+import com.watering.entity.DTO.ResponseDTO;
+import com.watering.entity.DepartmentEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+    @RequestMapping("department")
+    public ResponseDTO searchAllDepartment(){
+        ArrayList<DepartmentEntity> entities = new ArrayList<>();
+        entities.add(new DepartmentEntity(1, new Date(), "人事部", 1));
+        entities.add(new DepartmentEntity(2, new Date(), "研发部", 1));
+        entities.add(new DepartmentEntity(3, new Date(), "公关部", 1));
+        entities.add(new DepartmentEntity(4, new Date(), "市场部", 1));
+        return ResponseDTO.succData(entities);
+    }
+}
